5e40e5476b5f753ea8300a6f4f467dd8f1bb98b0
front-ed <edwinschutjes@gmail.com>
2023-03-25 14:08:32 +0100

    add API functionality

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
 /* ---- TARGETS && INITIALIZERS ---- */
+const url = "https://icanhazdadjoke.com/";
+const btn = getElement(".btn");
+const result = getElement(".result");
+
 /* ---- SCRIPT ---- */
+
+btn.addEventListener("click", () => {
+  fetchDadJoke();
+});
+
 /* ---- FUNCTIONS ---- */
 
-console.log("hello world");
+// Fetch dad joke from API
+const fetchDadJoke = async () => {
+  const response = await fetch(url, {
+    headers: {
+      Accept: "application/json",
+      "User-Agent": "educational",
+    },
+  });
+  // console.log(response);
+  const data = await response.json();
+  result.textContent = data.joke;
+};
 
 //-- Get DOM element function
 function getElement(selector) {
